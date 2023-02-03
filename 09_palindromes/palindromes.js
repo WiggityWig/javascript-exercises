cec1e0221534eddf4a87d18dec3c4d7e8020f850
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-03 13:32:05 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let lowerCase = string.toLowerCase();
+    let punctuationLess = lowerCase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let finalString = punctuationLess.replaceAll(" ",'');
+    let str = finalString;
+    const characterArray = str.split("");
+    let reverseArray = characterArray.reverse();
+    let reversedStr = reverseArray.join("");
+    if (((finalString > reversedStr) == false) && ((finalString < reversedStr) == false)) {
+        return true;
+    }
+        else {
+            return false;
+        }    
+}
 
 // Do not edit below this line
 module.exports = palindromes;
