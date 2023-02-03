1691e81ee466a0c6ddc925c674d7236a2b472de8
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-03 12:09:12 -0500

    update functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,68 @@
-const add = function() {
-	
-};
+//takes the sum of two numbers
+const add = function(firstNum, secondNum) {
+  let sumTotal = 0;
+  if (firstNum.length == 0) {
+      return 0;
+  } 
+      else if (firstNum.length >0){
+          firstNum.forEach(item =>{
+              sumTotal += item;
+          })
+          return sumTotal;
+      }  
+      else {sumTotal =+ firstNum + secondNum;
+          return sumTotal;
+      }
+}
+//same as add
+const sum = function(firstNum, secondNum) {
+  let sumTotal = 0;
+  if (firstNum.length == 0) {
+      return 0;
+  } 
+      else if (firstNum.length >0){
+          firstNum.forEach(item =>{
+              sumTotal += item;
+          })
+          return sumTotal;
+      }  
+      else {sumTotal =+ firstNum + secondNum;
+          return sumTotal;
+      }
+}
+//subracts seconds number from first number
+const subtract = function (firstNum,secondNum) {
+  let subtractTotal = firstNum - secondNum;
+  return subtractTotal;
+}
+//multiplies first number by second number, or multiple numbers as an array
+const multiply = function(firstNum, secondNum){
+  let sumTotal = 1;
+  if ((firstNum.length >0)){
+      firstNum.forEach(item => {
+          sumTotal *= item;
+      })
+      return sumTotal;
+  }
+  else {
+      sumTotal *= firstNum *secondNum;
+      return sumTotal;
+  }  
+}
+//takes the first argument in the function and raises it to the second argument power
+const power = function (number, power){
+  let total = (number **= power);
+  return total;
+}
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+//finds the factorial of a number
+const factorial = function (number){
+  let total = 1
+  for(let i = 1; i <= number; i++){
+      total *= i;
+  }
+  return total;
+}
 // Do not edit below this line
 module.exports = {
   add,
