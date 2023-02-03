23f6352e0435cb9e3e44bfb38d2c958e268e5bed
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-03 15:56:10 -0500

    create fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number){
+    let i =0 
+    Number(number);
+    const array = [0,1,1];
+    if (number >0){
+        for ( i = 3; i <= 25; i++) {
+            array[i] = array[i-2] + array[i-1];
+        }
+            return array[number];
+            } 
+            else if (number < 0) {
+                return "OOPS";
+            }
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
