530fb9cab853937aa5f1823af3fe5226b6648887
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-04 09:33:03 -0500

    solve function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(bookArray) {
+    let titleArray = [];
+    for (let i = 0; i < (bookArray.length); i++) {
+        let title = bookArray[i].title;
+        titleArray.push(title);
+    }
+    return titleArray;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
