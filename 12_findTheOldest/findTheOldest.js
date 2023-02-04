7a395907a480edfd0334493cfa2f1d3fe6130441
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-04 13:53:44 -0500

    create function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(pplArray) {
+    const agesArray = [];
+    for (let i = 0; i < pplArray.length; i ++) {
+        if (pplArray[i].yearOfDeath == undefined){
+            death = (new Date().getFullYear())
+        }
+            else {
+                death = (pplArray[i].yearOfDeath)
+            };
+        const birth = (pplArray[i].yearOfBirth);
+        const age = death - birth;
+        const name = (pplArray[i].name);
+      // const death = (pplArray[i].yearOfDeath);
+        agesArray.push({name, birth, death, age});
+         }
+    agesArray.sort(function(a,b){
+        let keyA = a.age;
+        let keyB = b.age;
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+        
+    })
+   let oldestArray = (agesArray.slice(-1));
+   let oldest = Object.assign({}, oldestArray);
+   return oldest[0];
 
+  
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
